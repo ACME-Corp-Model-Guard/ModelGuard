7f239adb7f3c67b6ed99b8e053de50ee0c44c3e7
schul236 <schul236@purdue.edu>
2025-10-26 22:57:51 -0500

    Remove obsolete files and align with new AWS arcitecture

---
+++ tem.py
@@ -0,0 +1,5 @@
+[33m98c4ea4[m[33m ([m[1;36mHEAD[m[33m -> [m[1;32mfeature/system-design-implementation[m[33m)[m Merge branch 'main' of https://github.com/rcaustin/ModelGuard into feature/system-design-implementation
+[33ma4d9b6e[m[33m ([m[1;31morigin/main[m[33m, [m[1;31morigin/HEAD[m[33m)[m Merge pull request #19 from rcaustin/setup-dependabot
+[33m3f241d0[m Configure dependabot
+[33m318e4a5[m Merge pull request #18 from rcaustin/7-setup-cicd-pipeline-with-github-actions
+[33mdbd37a6[m Changed pre-commit to autoformat
