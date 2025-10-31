dc92a570430c51cf9b420e0cee950636440b9f13
Chatfie <codyhatfield70@gmail.com>
2025-10-31 05:13:25 -0400

    Formatting

--- src/model.py
+++ src/model.py
@@ -15,7 +15,6 @@ from src.metrics.net_score import calculate_net_score
 import time  # high-resolution timing
 
 
-
 METRICS: list[_metrics.Metric] = [
     _metrics.AvailabilityMetric(),
     _metrics.BusFactorMetric(),
